e67b81b554c3f097b0cc12f0e215e30f82c3d3a3
terriblefire <stephen@terriblefire.com>
2017-10-02 17:55:12 +0100

    removed some test code

--- rtl/tf328_ram_top.v
+++ rtl/tf328_ram_top.v
@@ -66,7 +66,6 @@ wire GAYLE_REGS = (A[23:15] != {8'hDA, 1'b1});
 
 wire GAYLE_ID= (A[23:15] != {8'hDE, 1'b0});
 wire GAYLE_ACCESS = (GAYLE_ID & GAYLE_REGS) | AS20;
-wire TEST_ACCESS = ({A[23:16]} != {8'hE9}) | AS20;
 
 reg [2:0] POR = 3'b111;
 
@@ -161,15 +160,6 @@ always @(posedge CLKCPU) begin
 	 GAYLE_IDE_D <= GAYLE_IDE;
     intcycle_dout <= (~GAYLE_ACCESS) & RW20;
 	 
-	 if ((TEST_ACCESS | DS20 | ~ds_d) == 1'b0) begin
-		
-		if (RW20) begin 
-			test_reg <= test_reg + 'd1;
-		end else begin 
-			test_reg <= 8'h70;
-		end
-	 end
-
 end
 
 FDCP #(.INIT(1'b1)) 
@@ -190,13 +180,13 @@ FDCP #(.INIT(1'b1))
 		.PRE(AS20) // Asynchronous set input
 );
 
-assign PUNT = POR[2] | GAYLE_ACCESS & GAYLE_IDE & TEST_ACCESS & RAM_ACCESS & Z2_ACCESS  ? 1'bz : 1'b0;
+assign PUNT = POR[2] | GAYLE_ACCESS & GAYLE_IDE & RAM_ACCESS & Z2_ACCESS  ? 1'bz : 1'b0;
 assign IDECS = A[12] ? {GAYLE_IDE, 1'b1} : {1'b1, GAYLE_IDE}; 
 
 assign INT2 = GAYLE_INT2 ? 1'b0 : 1'bz;
 wire [7:4] data_out = GAYLE_ACCESS ? 4'b1111 : {gayle_dout,3'b000};
 
-assign D[7:0] = TEST_ACCESS ? ((intcycle_dout) ? {data_out[7:4], 4'h0} : 8'bzzzzzzzz) : test_reg;   
+assign D[7:0] = (intcycle_dout) ? {data_out[7:4], 4'h0} : 8'bzzzzzzzz;   
 
 assign DSACK[1] = FASTCYCLE ? 1'bz : 1'b0;
 assign DSACK[0] = (FASTCYCLE | RAM_ACCESS) ? 1'bz : 1'b0;
